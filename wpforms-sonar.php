687787e349628da03337d45eab1383a2509a8750
Nick Pratley <nick@npratley.net>
2023-06-03 00:17:22 +1000

    remove github updater

--- wpforms-sonar.php
+++ wpforms-sonar.php
@@ -22,20 +22,6 @@ if (!function_exists('is_plugin_active_for_network')) {
     require_once(ABSPATH . '/wp-admin/includes/plugin.php');
 }
 
-
-// We only want to run the updater if we are in the admin area.
-add_action('admin_init', function () {
-    if (!class_exists('GithubUpdater')) {
-        require_once __DIR__ . '/GithubUpdater.php';
-    }
-
-    GithubUpdater::make()
-        ->repository('the-it-dept/wpforms-sonar')
-        ->asset_name('wpforms-sonar.zip')
-        ->readme_url('https://raw.githubusercontent.com/the-it-dept/wpforms-sonar/main/README.md')
-        ->boot(__FILE__);
-});
-
 require_once __DIR__ . '/Sonar.php';
 // Boot the plugin.
 Sonar::make()
